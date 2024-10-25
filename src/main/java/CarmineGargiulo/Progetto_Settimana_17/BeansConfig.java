34e40374141026e4bc9b0bde7f4de84e311b4feb
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-10-25 10:31:24 +0200

    aggiunto faker

---
+++ src/main/java/CarmineGargiulo/Progetto_Settimana_17/BeansConfig.java
@@ -0,0 +1,10 @@
+package CarmineGargiulo.Progetto_Settimana_17;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeansConfig {
+
+
+}
