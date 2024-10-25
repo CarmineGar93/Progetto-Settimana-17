a55ff932f0d8f71f8b9af7db82e0401fb64be18e
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-10-25 10:33:31 +0200

    aggiunti nella classe di configurazione faker e scanner

--- src/main/java/CarmineGargiulo/Progetto_Settimana_17/BeansConfig.java
+++ src/main/java/CarmineGargiulo/Progetto_Settimana_17/BeansConfig.java
@@ -1,10 +1,22 @@
 package CarmineGargiulo.Progetto_Settimana_17;
 
+import com.github.javafaker.Faker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 @Configuration
 public class BeansConfig {
 
+    @Bean
+    public Faker getFaker() {
+        return new Faker(Locale.ITALY);
+    }
 
+    @Bean
+    public Scanner getScanner(){
+        return new Scanner(System.in);
+    }
 }
